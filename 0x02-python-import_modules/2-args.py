d0ab98cdef184d5a279ab04923bc0977d36a9b8c
Isachi25 <sharonachiengmasiga@gmail.com>
2024-01-04 10:21:03 -0800

    A program that prints the sum of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     arg_count = len(sys.argv) - 1
     print("{} argument{}{}"
             .format(arg_count, "" if arg_count == 1 else "s", "." if arg_count == 0 else ":"))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     arg_count = len(sys.argv) - 1
     sum = 0
-    for i in range(len):
+    for i in range(arg_count):
         sum += int(sys.argv[i + 1])
         print(sum)
