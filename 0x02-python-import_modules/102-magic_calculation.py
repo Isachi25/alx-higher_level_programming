39565944905527a12d98464f6e9162951c71e099
Isachi25 <sharonachiengmasiga@gmail.com>
2024-01-08 07:15:37 -0800

    A function that does exactly the same as the given python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
-        else:
-            return sub(a, b)
-        return 0
+        return c
+    else:
+        return sub(a, b)
+    return 0
