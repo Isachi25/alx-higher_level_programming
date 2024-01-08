cebab0357d7c36438299832a7fc8bae31ff067a0
Isachi25 <sharonachiengmasiga@gmail.com>
2024-01-08 06:59:53 -0800

    A prog that prints all the names defined by the compiled module given

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for 1 in dir(hidden_4):
-        if 1[0:2] != "__":
-            print(1)
+    for l in dir(hidden_4):
+        if l[0:2] != "__":
+            print(l)
