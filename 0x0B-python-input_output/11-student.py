37a3046cf9e408b5a00890243d45e5b60f9e7d63
Isachi25 <sharonachiengmasiga@gmail.com>
2024-02-06 05:18:34 -0800

    Students to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student class Module"""
+
+
+class Student():
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets dict with filter"""
+        if type(attrs) is list and all([type(attr) == str for attr in attrs]):
+            return {key: val for key, val in self.__dict__.items() if key in
+                    attrs}
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """Method for loading attributes from json"""
+        for k, val in json.items():
+            self.__dict__[k] = val
