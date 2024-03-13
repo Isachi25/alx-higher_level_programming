450e3840846a978e872b77c01535897884c2377f
Isachi25 <sharonachiengmasiga@gmail.com>
2024-03-13 13:48:21 -0700

    Creates the MySQL server user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates user user_0d_1
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY "user_0d_1_pwd";
+
+-- grant priviledges
+GRANT  ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
