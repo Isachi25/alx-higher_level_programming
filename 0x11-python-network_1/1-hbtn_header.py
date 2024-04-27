eaf7f75beb335c58db6940a866425b3f6b237a75
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 10:29:41 -0700

    Takes in a URL, sends a request to the URL, and displays the value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Takes URL, sends request, and displays value of X-Request-Id"""
-import urllib.request
+"""
+Takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response
+"""
 import sys
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        x_request_id = response.getheader("X-Request-Id")
-        print(x_request_id)
 
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
