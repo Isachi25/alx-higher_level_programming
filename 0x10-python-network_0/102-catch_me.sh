2ff998d810d19ae18f35629ac855912be75152d5
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 10:10:57 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# causes server to respond with a certain msg
-curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
-
+# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
