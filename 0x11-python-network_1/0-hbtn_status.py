1ea67f738df14e5c6e634869af7f33c84fe7328c
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 10:25:15 -0700

    Fetches a given url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status with urlib"""
-from urllib import request
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
 
-if __name__ == '__main__':
-    req = request.Request("https://alx-intranet.hbtn.io/status")
-    with request.urlopen(req) as response:
-        r = response.read()
-        s = "Body response:\n\t- type: {}\n\t- content: {}"
-        s += "\n\t- utf8 content: {}"
-        print(s.format(type(r), r, r.decode('utf-8')))
 
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
