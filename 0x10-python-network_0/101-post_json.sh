7dfdac47705ab76e1ca47780513ffdb6f8853950
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 10:05:19 -0700

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# posts json file
-curl -s -X POST -d @"$2" -H "Content-Type: application/json" "$1"
-
+# Sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
