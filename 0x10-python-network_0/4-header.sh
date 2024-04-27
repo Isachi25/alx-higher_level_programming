27ed4bcaeb4f891fb96ae940191ae4e95c927471
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 09:25:45 -0700

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends GET req with header var
-curl -s -H "X-School-User-Id:98" "$1"
-
+# Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
