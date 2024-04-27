7e4e1b65aaa8e6ccb236cbd26ef4451bb1ed62e1
Isachi25 <sharonachiengmasiga@gmail.com>
2024-04-27 13:22:49 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and
+displays the body of the response
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
